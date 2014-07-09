0ff0be0c1f3499c08a0fb3ae67633a02e37d2a15
donatien2020 <m.donatien@pivotaccess.com>
2014-07-09 19:26:03 +0200

    ibisha

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -3,26 +3,23 @@ import java.util.Properties;
 import play.*;
 import play.jobs.*;
 import play.test.*;
-
-import models.*;
-
 @OnApplicationStart
 public class Bootstrap extends Job {
 	public void doJob() {
-		if(Play.configuration.getProperty("application.mode").equals("prod")){
-			if (Operator.count() == 0) {
-				Fixtures.loadModels("initial-data-prod.yml");
-			}
-		}else{
-					if (Operator.count() == 0) {
-				Fixtures.loadModels("initial-data.yml");
-
-			} else {
-				Fixtures.deleteAllModels();
-				Fixtures.loadModels("initial-data.yml");
-			}
-	
-		}
+//		if(Play.configuration.getProperty("application.mode").equals("prod")){
+//			if (Operator.count() == 0) {
+//				Fixtures.loadModels("initial-data-prod.yml");
+//			}
+//		}else{
+//					if (Operator.count() == 0) {
+//				Fixtures.loadModels("initial-data.yml");
+//
+//			} else {
+//				Fixtures.deleteAllModels();
+//				Fixtures.loadModels("initial-data.yml");
+//			}
+//	
+//		}
 		
 	}
 }
