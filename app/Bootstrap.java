baf39eccc1a603f07b2ee76af2d40c5fb16d2bc0
donatien2020 <m.donatien@pivotaccess.com>
2014-07-27 08:06:55 +0200

    ggg

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -8,20 +8,20 @@ import play.test.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
 	public void doJob() {
-		if(Play.configuration.getProperty("application.mode").equals("prod")){
-			if (Operator.count() == 0) {
-				Fixtures.loadModels("initial-data-prod.yml");
-			}
-		}else{
-					if (Operator.count() == 0) {
-				Fixtures.loadModels("initial-data.yml");
-
-			} else {
-				Fixtures.deleteAllModels();
-				Fixtures.loadModels("initial-data.yml");
-			}
-	
-		}
+//		if(Play.configuration.getProperty("application.mode").equals("prod")){
+//			if (Operator.count() == 0) {
+//				Fixtures.loadModels("initial-data-prod.yml");
+//			}
+//		}else{
+//					if (Operator.count() == 0) {
+//				Fixtures.loadModels("initial-data.yml");
+//
+//			} else {
+//				Fixtures.deleteAllModels();
+//				Fixtures.loadModels("initial-data.yml");
+//			}
+//	
+//		}
 		
 	}
 }
